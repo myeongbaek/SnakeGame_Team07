043b538645fe1b0fe2e544c831032e99295ef529
horrorkist <horrorkist@gmail.com>
2022-05-08 11:47:36 +0900

    feat/GenerateFruitPositionDual function

--- utils.js
+++ utils.js
@@ -13,6 +13,20 @@ export function GenerateFruitPosition(trail, tileCount) {
   return { x, y };
 }
 
+export function GenerateFruitPositionDual(trail, tileCount) {
+  let x, y;
+  while (true) {
+    x = Math.floor(Math.random() * tileCount.x);
+    y = Math.floor(Math.random() * tileCount.y);
+
+    let flag = true;
+    for (var i = 0; i < trail.length; i++) {
+      if (x == trail[i].x && y == trail[i].y) flag = false;
+    }
+    if (flag) break;
+  }
+  return { x, y };
+}
 
 export function CompareRank(r1, r2) {
   return r2.score - r1.score;
